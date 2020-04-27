089985c7095b5468cb00e634adee9be9678b5162
ayumestika17 <62170693+ayumestika17@users.noreply.github.com>
2020-04-28 06:51:35 +0700

    Add files via upload

---
+++ operator.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body bgcolor= "#03e8fc">
+<?php
+
+$a =4;
+$b =12;
+
+$tambah	=$a+$b;
+$kurang	=$a-$b;
+$kali	=$a*$b;
+$bagi	=$a/$b;
+$modulo =$a%$b;
+
+
+echo "Tambah  ".$tambah."<br>";
+echo "kurang  ".$kurang."<br>";
+echo "kali  ".$kali."<br>";
+echo "bagi  ".$bagi."<br>";
+echo "modulo  ".$modulo."<br>";
+
+?>
+</body>
---
+++ variabel.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body bgcolor= "#03c2fc">
+<?php
+
+$a = 10;
+$b ="Saya adalah php";
+
+echo "Ini adalah Variabel A :".$a."<br>";
+echo "Ini adalah Variabel B : $b";
+
+
+?>
+</body>
